c41f3b2b2c70cca491732b818fd8b384d2974f27
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-08-02 21:48:08 +1000

    Formatting

--- src/data/tetromino.ts
+++ src/data/tetromino.ts
@@ -4,60 +4,60 @@ const tetrominoConfigs = [
     {
         colour: "gold",
         cells: [
-            [1,1],
-            [1,1]
+            [1, 1],
+            [1, 1]
         ]
     },
     {
         colour: "green",
         cells: [
-            [0,1,1],
-            [1,1,0]
+            [0, 1, 1],
+            [1, 1, 0]
         ]
     },
     {
         colour: "red",
         cells: [
-            [1,1,0],
-            [0,1,1]
+            [1, 1, 0],
+            [0, 1, 1]
         ]
     },
     {
         colour: "orange",
         cells: [
-            [1,1,1],
-            [1,0,0]
+            [1, 1, 1],
+            [1, 0, 0]
         ]
     },
     {
         colour: "blue",
         cells: [
-            [1,1,1],
-            [0,0,1]
+            [1, 1, 1],
+            [0, 0, 1]
         ]
     },
     {
         colour: "purple",
         cells: [
-            [1,1,1],
-            [0,1,0]
+            [1, 1, 1],
+            [0, 1, 0]
         ]
     },
     {
         colour: "teal",
         cells: [
-            [1,1,1,1]
+            [1, 1, 1, 1]
         ]
     },
-    
-  ]
+
+]
 
 export interface Tetromino {
-  colour: string,
-  cells: Grid<number>
+    colour: string,
+    cells: Grid<number>
 }
 
-export const tetrominoes: Tetromino[] = tetrominoConfigs.map(({cells, ...config}) => {
+export const tetrominoes: Tetromino[] = tetrominoConfigs.map(({ cells, ...config }) => {
     return {
         ...config,
         cells: from2dArray(cells)
